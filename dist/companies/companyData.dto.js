d26625316adbcf506784accb7efe62328a3c3227
Dmitry Shirokov <56573095+YoWuzzup@users.noreply.github.com>
2023-10-26 07:29:54 +0800

    Delete dist directory

--- dist/companies/companyData.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CompanyDataDto = void 0;
-class CompanyDataDto {
-}
-exports.CompanyDataDto = CompanyDataDto;
-//# sourceMappingURL=companyData.dto.js.map
--- dist/users/userData.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserDataDto = void 0;
-class UserDataDto {
-}
-exports.UserDataDto = UserDataDto;
-//# sourceMappingURL=userData.dto.js.map
