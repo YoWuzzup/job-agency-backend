e0545f1ff48a1303132d8208c1e59840284554ee
YoWuzzup <YoWuzzup@live.com>
2023-10-27 00:08:59 +0200

    done

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: `${process.env.FRONTEND_URL}`,
+    origin: `${configService.get('FRONTEND_URL')}`,
     credentials: true,
   });
 
