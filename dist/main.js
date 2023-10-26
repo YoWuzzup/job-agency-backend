9e49f159f945a41680f98ee41e3f7ea36ef0f9ae
YoWuzzup <YoWuzzup@live.com>
2023-10-26 23:43:29 +0200

    done

--- dist/main.js
+++ dist/main.js
@@ -9,7 +9,10 @@ async function bootstrap() {
     const configService = app.get(config_1.ConfigService);
     const PORT = configService.get('PORT') || 5000;
     app.use(cookieParser());
-    app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+    app.enableCors({
+        origin: `${process.env.FRONTEND_URL}`,
+        credentials: true,
+    });
     await app.listen(PORT, () => console.log(`The server is running on PORT: ${PORT}`));
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: `${configService.get('FRONTEND_URL')}`,
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true,
   });
 
